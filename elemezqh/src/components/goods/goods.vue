225b927afc3bc0e38cf3bc2b7638a02d7efe5cdf
zhuqihui <zhuqihui>
2018-04-18 08:56:03 +0800

    商品列表实现

--- elemezqh/src/components/goods/goods.vue
+++ elemezqh/src/components/goods/goods.vue
@@ -2,15 +2,38 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item " >
           <span class="text">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list food-list-hook">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img :src="food.icon" alt="" width="57" height="57">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
 
     </div>
-    <div class="foods-wrapper"></div>
   </div>
 </template>
 
@@ -61,6 +84,14 @@
         width : 56px
         padding : 0 12px
         line-height : 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
         .icon
           display : inline-block
           vertical-align : top
@@ -87,4 +118,49 @@
           font-size : 12px
     .foods-wrapper
       flex : 1
+      .title
+        padding-left : 14px
+        height : 26px
+        line-height : 26px
+        border-left : 2px solid #d9dde1
+        font-size : 12px
+        color : rgb(147,153,159)
+        background-color : #f3f5f7
+      .food-item
+        display : flex
+        margin : 18px
+        padding-bottom : 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .icon
+          flex : 0 0 57px
+          margin-right : 10px
+        .content
+          flex : 1
+          .name
+            margin: 2px 0 8px 0
+            height : 14px
+            line-height : 14px
+            font-size : 14px
+            color : rgb(7,17,27)
+          .desc, .extra
+            line-height : 10px
+            font-size : 10px
+            color : rgb(147,153,159)
+          .desc
+            line-height: 12px
+            margin-bottom : 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            line-height 24px
+            .now
+              font-size : 14px
+              color : rgb(240,20,20)
+              font-weight : 700
+            .old
+              text-decoration : line-through
+              font-size : 10px
+              color : rgb(147,153,159)
+              font-weight : 700
 </style>
