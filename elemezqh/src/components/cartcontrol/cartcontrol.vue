37349b66dcf2264cee8ed962fe6eb1b1cf901f59
zhuqihui <370217035@qq.com>
2018-04-21 20:41:25 +0800

    添加购物车，动画完善

--- elemezqh/src/components/cartcontrol/cartcontrol.vue
+++ elemezqh/src/components/cartcontrol/cartcontrol.vue
@@ -50,13 +50,12 @@
       line-height : 24px
       font-size : 24px
       color: rgb(0, 160, 220)
-      transform: rotate(180deg)
       &.move-enter-active
         transition: all 0.4s linear
       &.move-leave-active
         transition: all 0.4s linear
       &.move-enter, &.move-leave-to
-        transform: translate3d(24px, 0, 0)
+        transform: translate3d(24px, 0, 0) rotate(180deg)
     .cart-count
       display : inline-block
       vertical-align: top
